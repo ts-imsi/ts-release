85cc0b1dffeb3fa4a32002a20908e86dba90d52e
luoyunsak <sakfi102012@163.com>
2018-02-09 16:15:08 +0800

    #演示文件修改

---
+++ src/main/java/com/transen/tsrelease/dao/TbModRelyMapper.java
@@ -0,0 +1,7 @@
+package com.transen.tsrelease.dao;
+
+import com.transen.tsrelease.model.TbModRely;
+import com.transen.tsrelease.util.MyMapper;
+
+public interface TbModRelyMapper extends MyMapper<TbModRely> {
+}
--- src/main/java/com/transen/tsrelease/dao/TwfDictMapper.java
+++
@@ -1,9 +0,0 @@
-package com.transen.tsrelease.dao;
-
-
-import com.transen.tsrelease.model.TwfDict;
-import com.transen.tsrelease.util.MyMapper;
-
-public interface TwfDictMapper extends MyMapper<TwfDict> {
-     int saveTwfDic(TwfDict twfDict);
-}
